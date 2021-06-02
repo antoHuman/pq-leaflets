a0b858948f55dfc63df5b2c5db614a698503c26c
antonio.sferruzza <antonio.sferruzza.business@gmail.com>
2021-06-02 23:27:01 +0200

    clean up and refactor pagination

--- src/components/PaginationCustom.tsx
+++ src/components/PaginationCustom.tsx
@@ -5,16 +5,19 @@ type TSetCurrentPage = React.Dispatch<React.SetStateAction<number>>;
 const PaginationEmpty: FC = () => <Pagination.Item disabled><span>&nbsp;&nbsp;</span></Pagination.Item>;
 const PaginationFirst: FC<{goFirst:  ()=>void, currentPage: number}> = ({goFirst, currentPage}) => {
   const page = 1;
-  return (
+  return (<>
     <Pagination.Item active={page === currentPage} onClick={goFirst}
                      key={'page-' + page}>{page}</Pagination.Item>
-  )
+    <Pagination.Ellipsis disabled/>
+  </>)
 };
 const PaginationLast: FC<{goLast:  ()=>void, currentPage: number, pageAmount: number}> = ({goLast, currentPage, pageAmount}) => {
   const page = pageAmount;
-  return (
+  return (<>
+    <Pagination.Ellipsis disabled/>
     <Pagination.Item active={page === currentPage} onClick={goLast}
                      key={'page-' + page}>{page}</Pagination.Item>
+  </>
   )
 };type TPaginationCustomProps = {setCurrentPage:  TSetCurrentPage, currentPage: number, pageAmount: number};
 
@@ -25,10 +28,12 @@ const PaginationCustom: FC<TPaginationCustomProps> = ({setCurrentPage, currentPa
   const goPrev = ()=> setCurrentPage(prevPage => (prevPage > 1 ? (prevPage - 1) : prevPage));
   const goFirst = ()=> setCurrentPage(1);
   const goLast = ()=> setCurrentPage(pageAmount);
-  const nextVisiblePages = 5;
-  const prevVisiblePages = 5;
+  const nextVisiblePages = 2;
+  const prevVisiblePages = 2;
   const totalAmountOfVisible = 2* extraVisiblePages + nextVisiblePages + prevVisiblePages + 1; // considering also ellipsis, first and last page
   const paginationArray = [];
+  let hasPaginationFirst = false;
+  let hasPaginationLast = false;
   if (pageAmount <= totalAmountOfVisible) {
     for (let i = 0; i < totalAmountOfVisible; i++) {
       const page = 1 + i;
@@ -36,62 +41,55 @@ const PaginationCustom: FC<TPaginationCustomProps> = ({setCurrentPage, currentPa
         paginationArray.push(<Pagination.Item active={page === currentPage} onClick={() => setCurrentPage(page)}
                                               key={'page-' + page}>{page}</Pagination.Item>)
       } else {
-        paginationArray.push(<PaginationEmpty/>)
-      }
-    }
-  } else if (pageAmount === totalAmountOfVisible + 1) {
-    // only one extra -> always only one ellipsis;
-    const amountHidden = pageAmount - totalAmountOfVisible + 1; // one to make space for the ellipsis + the extra one
-    if (currentPage < pageAmount - (extraVisiblePages + prevVisiblePages)) {
-      for (let i = 0; i < (totalAmountOfVisible - extraVisiblePages); i++) {
-        const page = i + extraVisiblePages - 1; // no ellipsis
-        paginationArray.push(<Pagination.Item active={page === currentPage} onClick={()=>setCurrentPage(page)} key={'page-' + page}>{page}</Pagination.Item>)
-      }
-      paginationArray.push(<Pagination.Ellipsis disabled/>);
-      paginationArray.push(<PaginationLast goLast={goLast} currentPage={currentPage} pageAmount={pageAmount} />);
-    } else {
-      paginationArray.push(<PaginationFirst goFirst={goFirst} currentPage={currentPage} />);
-      paginationArray.push(<Pagination.Ellipsis disabled/>);
-      for (let i = 0; i < (totalAmountOfVisible - extraVisiblePages); i++) {
-        const page = i + 1 + amountHidden + 1; // start after the first page + the 2 hidden
-        paginationArray.push(<Pagination.Item active={page === currentPage} onClick={()=>setCurrentPage(page)} key={'page-' + page}>{page}</Pagination.Item>)
+        paginationArray.push(<PaginationEmpty key={'page-empty-' + page}/>)
       }
     }
   } else {
-    // large amount of pages, from 1 to 2 ellipsis needed
-    if (currentPage <= extraVisiblePages + prevVisiblePages + 1) {
-      for (let i = 0; i < (nextVisiblePages + 1 + prevVisiblePages + extraVisiblePages); i++) {
-        const page = i + extraVisiblePages - 1; // no ellipsis
-        paginationArray.push(<Pagination.Item active={page === currentPage} onClick={()=>setCurrentPage(page)} key={'page-' + page}>{page}</Pagination.Item>)
+    let pagesArrayLength: number;
+    let startingPage: number;
+    if (pageAmount === totalAmountOfVisible + 1) {
+      // only one extra -> always only one ellipsis;
+      const amountHidden = pageAmount - totalAmountOfVisible + 1; // one to make space for the ellipsis + the extra one
+      pagesArrayLength = totalAmountOfVisible - extraVisiblePages;
+      if (currentPage < pageAmount - (extraVisiblePages + prevVisiblePages)) {
+        startingPage = extraVisiblePages - 1; // no ellipsis
+        hasPaginationLast = true;
+      } else {
+        hasPaginationFirst = true;
+        startingPage = 1 + amountHidden + 1; // start after the first page + the 2 hidden
       }
-      paginationArray.push(<Pagination.Ellipsis disabled/>);
-      paginationArray.push(<PaginationLast goLast={goLast} currentPage={currentPage} pageAmount={pageAmount} />);
-    } else if (currentPage < pageAmount - (extraVisiblePages + nextVisiblePages)) {
-      // double ellipsis
-      paginationArray.push(<PaginationFirst goFirst={goFirst} currentPage={currentPage} />);
-      paginationArray.push(<Pagination.Ellipsis disabled/>);
-
-      for (let i = 0; i < (totalAmountOfVisible - 2*extraVisiblePages); i++) {
-        const page = i + currentPage - prevVisiblePages;
-        paginationArray.push(<Pagination.Item active={page === currentPage} onClick={()=>setCurrentPage(page)} key={'page-' + page}>{page}</Pagination.Item>)
-      }
-      paginationArray.push(<Pagination.Ellipsis disabled/>);
-      paginationArray.push(<PaginationLast goLast={goLast} currentPage={currentPage} pageAmount={pageAmount} />);
     } else {
-      paginationArray.push(<PaginationFirst goFirst={goFirst} currentPage={currentPage} />);
-      paginationArray.push(<Pagination.Ellipsis disabled/>);
+      // large amount of pages, from 1 to 2 ellipsis needed
+      if (currentPage <= extraVisiblePages + prevVisiblePages + 1) {
+        pagesArrayLength = nextVisiblePages + 1 + prevVisiblePages + extraVisiblePages;
+        startingPage = extraVisiblePages - 1; // no ellipsis
+        hasPaginationLast = true;
+      } else if (currentPage < pageAmount - (extraVisiblePages + nextVisiblePages)) {
+        // double ellipsis
+        hasPaginationFirst = true;
+        hasPaginationLast = true;
+        pagesArrayLength = totalAmountOfVisible - 2 * extraVisiblePages;
+        startingPage = currentPage - prevVisiblePages;
+      } else {
+        hasPaginationFirst = true;
+        pagesArrayLength = totalAmountOfVisible - extraVisiblePages;
+        startingPage = pageAmount - (extraVisiblePages + nextVisiblePages + prevVisiblePages);
 
-      for (let i = 0; i < (totalAmountOfVisible - extraVisiblePages); i++) {
-        const page = i + pageAmount - (extraVisiblePages + nextVisiblePages + prevVisiblePages);
-        paginationArray.push(<Pagination.Item active={page === currentPage} onClick={()=>setCurrentPage(page)} key={'page-' + page}>{page}</Pagination.Item>)
       }
+    }
+    for (let i = 0; i < pagesArrayLength; i++) {
+      const page = i + startingPage;
+      paginationArray.push(<Pagination.Item active={page === currentPage} onClick={() => setCurrentPage(page)}
+                                            key={'page-' + page}>{page}</Pagination.Item>)
     }
   }
   return (
     <Pagination>
-      <Pagination.Prev onClick={goPrev} disabled={currentPage === 1}/>
+      <Pagination.Prev onClick={goPrev} disabled={currentPage === 1} />
+      {hasPaginationFirst && <PaginationFirst goFirst={goFirst} currentPage={currentPage} />}
       {paginationArray}
-      <Pagination.Next onClick={goNext} disabled={currentPage === pageAmount}/>
+      {hasPaginationLast && <PaginationLast goLast={goLast} currentPage={currentPage} pageAmount={pageAmount} />}
+      <Pagination.Next onClick={goNext} disabled={currentPage === pageAmount} />
     </Pagination>
   )
 }
